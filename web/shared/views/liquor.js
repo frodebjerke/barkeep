f3359e1797587f7ecaa41e55b8d05060a78937b9
Frode Bjerke <frode.bjerke@gmail.com>
2014-07-25 13:50:50 +0200

    display liquor according to new schema

--- web/shared/views/liquor.js
+++ web/shared/views/liquor.js
@@ -1,10 +1,11 @@
 (function (bke) {
   bke.views.liquor = function (liquor) {
+    console.log(liquor)
     return m(".el-liquor", {
       onclick: function () {
         m.route("/addbottle/"+ liquor._id);
       },
-      style: "background-image:url('"+liquor.images.external+"');"
+      style: liquor.images ? "background-image:url('"+liquor.images.large+"');" : ""
     },m(".liquor-info", [
       m(".liquor-name", liquor.name)
     ]));
