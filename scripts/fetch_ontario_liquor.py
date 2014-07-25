2c51db7de44b8640f87edfdd3b6b4f019ce66a8d
Frode Bjerke <frode.bjerke@gmail.com>
2014-07-25 13:41:35 +0200

    group liquors by name. Specific product info as size and price in an array

--- scripts/fetch_ontario_liquor.py
+++ scripts/fetch_ontario_liquor.py
@@ -8,15 +8,9 @@ base_url = "http://www.lcboapi.com"
 #url = "mongodb://barkeep:en gang i blandt@kahana.mongohq.com:10085/villevillavika"
 client = pymongo.MongoClient()
 
-def transform(liquor):
+def liquor(liquor):
   name = liquor['name']
   updated_at = datetime.datetime.now()
-
-  price = {
-    'country': 'canada',
-    'date': datetime.datetime.now(),
-    'price': liquor['regular_price_in_cents'] / 100 * 5
-  }
 
   category = {
     'primary': liquor['primary_category'],
@@ -26,6 +20,7 @@ def transform(liquor):
     'varietal': liquor['varietal'],
     'tags': liquor['tags'].split(" ")
   }
+  category = dict((k, v) for k, v in category.iteritems() if v)
 
   contents = {
     'sugar': liquor['sugar_content'],
@@ -33,44 +28,59 @@ def transform(liquor):
     'alcohol_content': liquor['alcohol_content']
   }
 
-  images = {
-    'external': liquor['image_url'],
-    'external_thumb': liquor['image_thumb_url']
-  }
+  contents = dict((k, v) for k, v in contents.iteritems() if v)
 
-  packaging = {
-    'size_ml': liquor['volume_in_milliliters'],
-    'package': liquor['package']
+  images = {
+    'large': liquor['image_url'],
+    'thumb': liquor['image_thumb_url']
   }
+  images = dict((k, v) for k, v in images.iteritems() if v)
 
   about = {
     'origin': liquor['origin'],
     'producer': liquor['producer_name']
   }
+  about = dict((k, v) for k, v in about.iteritems() if v)
 
   notes = {
     'serving_suggestion': liquor['serving_suggestion'],
     'description': liquor['description'],
     'tasting_note': liquor['tasting_note']
   }
+  notes = dict((k, v) for k, v in notes.iteritems() if v)
 
-  return {
+  ret = {
     'name': name,
     'updated_at': updated_at,
-    'price': [price],
-    'category': category,
-    'images': images,
-    'contents': contents,
-    'packaging': packaging,
-    'about': about,
-    'notes': notes
+  }
+
+  if len(category) > 0:
+    ret['category'] = category
+  if len(images) > 0:
+    ret['images'] = images
+  if len(contents) > 0:
+    ret['contents'] =  contents
+  if len(about) > 0:
+    ret['about'] = about
+  if len(notes) > 0:
+    ret['notes'] = notes
+  return ret
+
+def product(liquor):
+  return {
+    'size_ml': liquor['volume_in_milliliters'],
+    'material': liquor['package_unit_type'],
+    'country': 'Canada',
+    'date': datetime.datetime.now(),
+    'price': liquor['regular_price_in_cents'] / 100 * 5,
+    'units': liquor['total_package_units']
   }
 
 def parsePage(page):
   data = json.loads(page)
   if (data['status'] == 200):
     for b in data["result"]:
-      client['villevillavika']['liquor'].update({"id":b ['id']},{"$set": transform(b)}, upsert=True)
+      client['villevillavika']['liquor'].update({'name': b['name']},{"$push": {'products': product(b)}, "$set": liquor(b)}, upsert=True)
     return data['pager']['next_page_path']
   else:
     raise Exception('status code', data['status'])
