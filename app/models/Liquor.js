b3e13bdaeb166b83c64a8aad33bab2f3c00e2168
Frode Bjerke <frode.bjerke@gmail.com>
2014-07-25 13:51:06 +0200

    new schema

--- app/models/Liquor.js
+++ app/models/Liquor.js
@@ -1,52 +1,34 @@
 var mongoose = require('mongoose');
 
 var Liquor = mongoose.Schema({
-  // product info
-  origin: String,
-  updated_at: Date,
-  stock_type: String,
-  producer_name: String,
-  is_discontinued: Boolean,
-  is_dead: Boolean,
-  product_no: Number,
-  image_thumb_url: String,
-  image_url: String,
   name: String,
-
-
-  // category
-  secondary_category: String,
-  primary_category: String,
-  tertiary_category: String,
-  style: String,
-  varietal: String,
-
-  // metrics
-  package_unit_type: String,
-  package_unit_volume_in_milliliters: Number,
-  total_package_units: Number,
-  volume_in_milliliters: Number,
-  alcohol_content: Number,
-  package: String,
-
-  // ingredients
-  sugar_content: String,
-  sugar_in_grams_per_liter: Number,
-  is_kosher: Boolean,
-
-
-  // price
-  price_per_liter_of_alcohol_in_cents: Number,
-  price_per_liter_in_cents: Number,
-  regular_price_in_cents: Number,
-
-  // meta-meta
-  serving_suggestion: String,
-  description: String,
-  tags: String,
-  tasting_note: String,
-
-
+  about: {
+    producer: String,
+    origin: String
+  },
+  images: {
+    external: String,
+    external_thumb: String
+  },
+  category: {
+    primary: String,
+     secondary: String,
+     tertiary: String,
+     style: String,
+     varietal: String,
+     tags: Array
+  },
+  notes: {
+    serving_suggestion: String,
+    tasting_note: String,
+    description: String
+  },
+  contents: {
+    kosher: Boolean,
+    alcohol_content: Number,
+    sugar: String
+  },
+  products: Array
 }, {
   collection: "liquor"
 });
