1556210fc7bef9a137d3470b638427fa7d1ed556
Frode Bjerke <frode.bjerke@gmail.com>
2014-11-06 19:11:57 +0100

    removed unecessary components on landing

--- web/apps/landing/landing.js
+++ web/apps/landing/landing.js
@@ -1,14 +1,10 @@
 var m = require('mithril');
-var personstats = require('./personstats');
-var nlastdrinks = require('./nlastdrinks');
 var nlastbottles = require('./nlastbottles');
 var drinkorbottle = require('./drinkorbottle');
 
 module.exports = {
   controller: function () {
     return {
-      personstats: new personstats.controller(),
-      lastdrinks: new nlastdrinks.controller(3),
       lastbottles: new nlastbottles.controller(18),
       drinkorbottle: new drinkorbottle.controller()
     };
@@ -16,8 +12,7 @@ module.exports = {
   view: function (ctrl) {
     return m("", [
       drinkorbottle.view(ctrl.drinkorbottle),
-      personstats.view(ctrl.personstats),
-      nlastbottles.view(ctrl.lastbottles),
+      nlastbottles.view(ctrl.lastbottles)
     ]);
   }
 };
