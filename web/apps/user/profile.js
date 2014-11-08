a1923cf4c03cb844463641db2204ea7b8f0146cb
Frode Bjerke <frode.bjerke@gmail.com>
2014-11-08 18:11:49 +0100

    (feat) added users bottles to profile

--- web/apps/user/profile.js
+++ web/apps/user/profile.js
@@ -1,14 +1,25 @@
 var m = require('mithril');
+var _ = require('lodash');
 
 var User = require('../../models/User');
+var Bottle = require('../../models/Bottle');
+
+var bottleView = require('../../shared/views/bottle');
 
 module.exports = {
   controller: function () {
     var userid = m.route.param("bottle");
     this.user = m.prop({});
+    this.bottles = m.prop([]);
 
     var userfetch = userid ? User.getById(userid) : User.getMe();
-    userfetch.then(this.user);
+    userfetch
+      .then(this.user)
+      .then(Bottle.getAll)
+      .then(function (bottles) {
+        return filterUser(this.user().id(), bottles);
+      }.bind(this))
+      .then(this.bottles);
   },
   view: function (ctrl) {
     return m("", [
@@ -21,9 +32,22 @@ module.exports = {
           m(".u-grid-flex--box", [
             m(".text-title", "Cash balance"),
             m("", ctrl.user().balance() + ",-")
+          ]),
+          m(".u-grid-flex--box", [
+            m(".text-title", "# bottles"),
+            m("", ctrl.bottles().length)
           ])
-        ])
+        ]),
+        ctrl.bottles().map(function (bottle) {
+          return m(".u-grid--box.col-xs-12.col-sm-4.col-md-3.col-lg-2", bottleView(bottle));
+        })
       ])
     ]);
   }
 };
+
+function filterUser(userid, bottles) {
+  return _.filter(bottles, function (bottle) {
+    return bottle.owner_id() === userid;
+  });
+}
