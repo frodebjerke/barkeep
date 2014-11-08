a22070c2391e6ce6dcd9f9181281d46cba8cd588
Frode Bjerke <frode.bjerke@gmail.com>
2014-11-08 22:52:12 +0100

    (bug) fixed typo type logic error in prop-share

--- web/lib/mithril/prop-share.js
+++ web/lib/mithril/prop-share.js
@@ -3,9 +3,9 @@ var m = require('mithril');
 var share = {};
 
 module.exports = function (key, val) {
-  share.key = share.key || m.prop();
+  share[key] = share[key] || m.prop();
 
-  if (val) share.key(val);
+  if (val) share[key](val);
 
-  return share.key;
+  return share[key];
 };
